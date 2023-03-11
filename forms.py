3c10c3bfb160e9be00774f01a6cdc0023c952e03
Turkish Coffee <alexxis.noyola@aduasis.com>
2023-03-11 14:03:55 -0600

    Initial Commit

---
+++ forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL, Email, Length
+from flask_ckeditor import CKEditorField
+
+
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+class RegistrationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+class CommentForm(FlaskForm):
+    body = CKEditorField("Add Comment", validators=[DataRequired()])
+    submit = SubmitField(label='Comment', id="add_comment")
